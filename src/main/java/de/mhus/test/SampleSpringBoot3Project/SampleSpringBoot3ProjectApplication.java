72a59679546d7081c61474475bf5b38b99a01896
Mike Hummel <mh@mhus.de>
2023-01-13 19:09:04 +0100

    add initial data

---
+++ src/main/java/de/mhus/test/SampleSpringBoot3Project/SampleSpringBoot3ProjectApplication.java
@@ -0,0 +1,13 @@
+package de.mhus.test.SampleSpringBoot3Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBoot3ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mhus/test/SampleSpringBoot3Project/SampleSpringBoot3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mhus.test.SampleSpringBoot3Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
