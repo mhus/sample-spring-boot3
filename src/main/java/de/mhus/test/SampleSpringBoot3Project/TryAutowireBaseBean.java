85bcd5af22c9900f9b44dc888c82831312e28159
Mike Hummel <mike@mhus.de>
2024-11-12 09:09:35 +0100

    feat: add prove that reflection is possible in native execution

--- src/main/java/de/mhus/test/SampleSpringBoot3Project/TryAutowireBaseBean.java
+++ src/main/java/de/mhus/test/SampleSpringBoot3Project/TryAutowireBaseBean.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 @Service
 public class TryAutowireBaseBean {
@@ -13,10 +14,27 @@ public class TryAutowireBaseBean {
 
     @PostConstruct
     private void init() {
-        System.out.println("TryAutowireBaseBean: PostConstruct");
+        System.out.println(">>> TryAutowireBaseBean: PostConstruct");
         var configurableObject = new TryAutowireConfigurable();
+
         applicationContext.getAutowireCapableBeanFactory().autowireBean(configurableObject);
-        System.out.println("TryAutowireBaseBean: TryAutowireConfigurable autowired " + configurableObject.isAutowired());
+        System.out.println("*** TryAutowireBaseBean: TryAutowireConfigurable autowired " + configurableObject.isAutowired());
+
+        System.out.println("<<< TryAutowireBaseBean: PostConstruct");
+        proveReflectionIsActive(configurableObject);
+    }
+
+    private void proveReflectionIsActive(TryAutowireConfigurable configurableObject) {
+        System.out.println(">>> TryAutowireBaseBean: proveReflectionIsActive");
+        try {
+            var m = ReflectionUtils.findMethod(TryAutowireConfigurable.class, "setTryAutowireOtherBean", TryAutowireOtherBean.class);
+            m.setAccessible(true);
+            m.invoke(configurableObject, applicationContext.getBean(TryAutowireOtherBean.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("*** TryAutowireBaseBean: TryAutowireConfigurable autowired " + configurableObject.isAutowired());
+        System.out.println("<<< TryAutowireBaseBean: proveReflectionIsActive");
     }
 
 
